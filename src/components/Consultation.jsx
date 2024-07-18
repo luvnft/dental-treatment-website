4400cdc0d539a789a04ade96fe5b9a15346e4628
Efekan EFE <efekanefos@gmail.com>
2024-07-18 13:13:51 +0300

    testimonials_without_slider&&consultation_done

--- src/components/Consultation.jsx
+++ src/components/Consultation.jsx
@@ -1,7 +1,65 @@
-import React from "react";
-
+import consultationPerson from "../assets/images/consultationImages/consultationPerson.png";
+import contactArrow from "../assets/images/headerImages/headerContactArrow.png";
+import "../assets/styles/consultation.css";
 const Consultation = () => {
-  return <div>Consultation</div>;
+  return (
+    <section className="consultationContent grid grid-cols-[0.4fr_0.6fr] place-items-center max-w-[1140px] mx-auto my-10 gap-5 rounded-[50px]">
+      <div className="consultationImgSide">
+        <img
+          src={consultationPerson}
+          alt="Dentist"
+          className="consultationImg w-full max-w-[484px] max-h-[502px]"
+        />
+      </div>
+      <div className="consultationFormSide">
+        <h2 className="sectionMainTitle text-center mb-6 font-semibold text-[36px]">
+          Free Consultation
+        </h2>
+        <form action="POST" className="consultationForm">
+          <div className="consultationFormInputs grid grid-cols-2 gap-5">
+            <input
+              type="text"
+              placeholder="Full Name*"
+              className="consultationInput"
+            />
+            <input
+              type="text"
+              placeholder="I'm interested in*"
+              className="consultationInput"
+            />
+            <input
+              type="email"
+              placeholder="Email*"
+              className="consultationInput"
+            />
+            <input
+              type="tel"
+              placeholder="Phone Number*"
+              className="consultationInput"
+            />
+          </div>
+
+          <div
+            className="consultationFormButtonContent w-full shadow-buttonShadow bg-secondary flex justify-center items-center gap-2 px-5 py-2
+           rounded-[30px] font-semibold h-[50px] mt-8"
+          >
+            <input
+              className="consultationFormButton text-[20px]"
+              type="submit"
+              value="Get a Free Consultation"
+            />
+            <div className="bookingButtonImgContent max-w-[15px] flex items-center justify-center">
+              <img
+                src={contactArrow}
+                alt="Arrow"
+                className="bookingButtonImg w-full"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Consultation;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,19 +12,28 @@ import "../assets/styles/hero.css";
 const Hero = () => {
   return (
     <>
-      <section className="heroContent max-w-[1200px] mx-auto px-3 flex justify-between items-start relative min-h-[55vh]">
+      <section className="heroContent max-w-[1200px] mx-auto px-3 flex justify-between items-start relative min-h-[70vh]">
         <div className="heroInfoContent w-full max-w-[669px] z-10">
           <h1 className="heroTitle text-[50px] font-bold pt-10">
-            Dental Excellence Starts Here: Your Guide to a Brighter, Healthier <br /> Smile
+            Dental Excellence Starts Here: Your Guide to a Brighter, Healthier{" "}
+            <br /> Smile
           </h1>
           <div className="bookingButtonContent">
             <button className="bookingButton bg-secondary flex items-center gap-2 px-5 py-2 rounded-[30px] font-semibold h-[50px] mt-5 relative">
               Booking Now
               <div className="bookingButtonImgContent max-w-[13px] flex items-center justify-center">
-                <img src={contactArrow} alt="Arrow" className="bookingButtonImg w-full" />
+                <img
+                  src={contactArrow}
+                  alt="Arrow"
+                  className="bookingButtonImg w-full"
+                />
               </div>
               <div className="bookingButtonEffectImgContent  absolute max-w-[80px] right-[-15px] z-[-1]">
-                <img src={heroButtonEffect} alt="Arrow" className="bookingButtonEffectImg w-full" />
+                <img
+                  src={heroButtonEffect}
+                  alt="Arrow"
+                  className="bookingButtonEffectImg w-full"
+                />
               </div>
             </button>
           </div>
@@ -38,34 +47,62 @@ const Hero = () => {
           {/* Free Consultation */}
           <div className="qualityBox">
             <div className="qualityBoxImgContent">
-              <img src={freeConsultation} alt="Free Consultation" className="qualityBoxImg" />
+              <img
+                src={freeConsultation}
+                alt="Free Consultation"
+                className="qualityBoxImg"
+              />
             </div>
             <h3 className="qualityBoxTitle">Free Consultation</h3>
-            <p className="qualityBoxDesc">We offer flexible appointment scheduling and free to accommodate your busy life</p>
+            <p className="qualityBoxDesc">
+              We offer flexible appointment scheduling and free to accommodate
+              your busy life
+            </p>
           </div>
           {/* Expert Dentist */}
           <div className="qualityBox">
             <div className="qualityBoxImgContent">
-              <img src={expertDentist} alt="Expert Dentist" className="qualityBoxImg" />
+              <img
+                src={expertDentist}
+                alt="Expert Dentist"
+                className="qualityBoxImg"
+              />
             </div>
             <h3 className="qualityBoxTitle">Expert Dentist</h3>
-            <p className="qualityBoxDesc">Our team of experienced dentists and dental professionals boasts years of expertise in various areas of dentistry</p>
+            <p className="qualityBoxDesc">
+              Our team of experienced dentists and dental professionals boasts
+              years of expertise in various areas of dentistry
+            </p>
           </div>
           {/* High User Rating */}
           <div className="qualityBox">
             <div className="qualityBoxImgContent">
-              <img src={highUserRating} alt="High User Rating" className="qualityBoxImg" />
+              <img
+                src={highUserRating}
+                alt="High User Rating"
+                className="qualityBoxImg"
+              />
             </div>
             <h3 className="qualityBoxTitle">High User Rating</h3>
-            <p className="qualityBoxDesc">We offer flexible appointment scheduling and free to accommodate your busy life</p>
+            <p className="qualityBoxDesc">
+              We offer flexible appointment scheduling and free to accommodate
+              your busy life
+            </p>
           </div>
           {/* Best Equipment */}
           <div className="qualityBox">
             <div className="qualityBoxImgContent">
-              <img src={bestEquipment} alt="Best Equipment" className="qualityBoxImg" />
+              <img
+                src={bestEquipment}
+                alt="Best Equipment"
+                className="qualityBoxImg"
+              />
             </div>
             <h3 className="qualityBoxTitle">Best Equipment</h3>
-            <p className="qualityBoxDesc">Our compassionate team is dedicated to making your dental experience as comfortable and stress-free as possible</p>
+            <p className="qualityBoxDesc">
+              Our compassionate team is dedicated to making your dental
+              experience as comfortable and stress-free as possible
+            </p>
           </div>
         </div>
       </section>
